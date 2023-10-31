684c155756c3a0785306fc59081fab2242d86c91
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-10-31 16:54:02 +0700

    Add files via upload

---
+++ Numbers23.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Numbers23{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[0] = new int[3];
+        myNumbers[0] = new int[1];
+    }
+}
